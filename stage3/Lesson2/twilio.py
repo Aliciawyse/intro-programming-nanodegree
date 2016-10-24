94926a3aabbfae07ecfdc34bc48c9846c35483bd
Alicia <aliciawyse@gmail.com>
2016-10-24 01:09:30 +0000

    correcting spelling error

--- stage3/Lesson2/twilio.py
+++ stage3/Lesson2/twilio.py
@@ -8,7 +8,7 @@ auth_token  = "{{ #private info }}"  # Your Auth Token from www.twilio.com/conso
 #rest.TwilioRestClient will call the init function defined in the class which creates an instance called client. 
 client = rest.TwilioRestClient(account_sid, auth_token)
 
-#Now we can do things defined in class TwilioRestClient, such as how to connect with Twilio to send a text message. 
+#Now we can do things defined in class TwilioRestClient, such as connecting with Twilio to send a text message. 
 message = client.messages.create(body="Hey! You might be the next Customer Success Advocate!",
     to="",    # Replace with your phone number
     from_="+19099067001") # My Twilio number
